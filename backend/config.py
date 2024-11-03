84f0b4ca4b36a27db391cd96521f381417ef5183
Filip.Lazar <filip.lazar@exe.sk>
2024-11-03 17:12:34 +0100

    Polishing the initial backend structure

--- backend/config.py
+++ backend/config.py
@@ -4,6 +4,6 @@ import os
 MONGODB_URI = 'mongodb://localhost:27017/'
 
 # Directory to store videos
-VIDEO_STORAGE_PATH = './videos/'
+VIDEO_STORAGE_PATH = './storage/videos/'
 if not os.path.exists(VIDEO_STORAGE_PATH):
     os.makedirs(VIDEO_STORAGE_PATH)
